0f3456d7ad4604f98dbf4aa9f2d33bc1a4e3c0c3
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-10-28 09:49:45 +0530

    Update main.py

--- JPMorgan Task 1/main.py
+++ JPMorgan Task 1/main.py
@@ -21,6 +21,7 @@ time.sleep(.300)
 process2 = subprocess.Popen(['python', 'client3.py'], cwd=os.getcwd(), preexec_fn=os.setsid)
 
 process2.wait()
+
 os.killpg(os.getpgid(process.pid), signal.SIGTERM)
 
 # FOR BONUS TASK 
