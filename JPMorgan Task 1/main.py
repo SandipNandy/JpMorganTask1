b8c28cf5c0e311862c3bbc6ec9757b3b596c850c
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-10-28 09:49:10 +0530

    Update main.py

--- JPMorgan Task 1/main.py
+++ JPMorgan Task 1/main.py
@@ -15,7 +15,6 @@ import time
 import signal
 os.chdir(os.getcwd()+'/jpm_module_1')
 process = subprocess.Popen(['python', 'server3.py'], cwd=os.getcwd(), preexec_fn=os.setsid)
-
 time.sleep(.300)
 
 process2 = subprocess.Popen(['python', 'client3.py'], cwd=os.getcwd(), preexec_fn=os.setsid)
