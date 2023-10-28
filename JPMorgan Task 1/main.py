6975409a927bb5cbf05bd577565b583c6d841a32
SandipNandy <59725621+SandipNandy@users.noreply.github.com>
2023-10-28 09:49:22 +0530

    Update main.py

--- JPMorgan Task 1/main.py
+++ JPMorgan Task 1/main.py
@@ -17,6 +17,7 @@ os.chdir(os.getcwd()+'/jpm_module_1')
 process = subprocess.Popen(['python', 'server3.py'], cwd=os.getcwd(), preexec_fn=os.setsid)
 time.sleep(.300)
 
+
 process2 = subprocess.Popen(['python', 'client3.py'], cwd=os.getcwd(), preexec_fn=os.setsid)
 process2.wait()
 os.killpg(os.getpgid(process.pid), signal.SIGTERM)
